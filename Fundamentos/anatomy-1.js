894442de72f19ba3eae71f4b0f97a966217c9d9e
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-12-27 10:14:27 -0300

    Iniciando os estudos de JavaScript funcional aprendendo a anatomia de uma função

---
+++ Fundamentos/anatomy-1.js
@@ -0,0 +1,20 @@
+//Function Declaration
+
+function HelloWorld(){
+    console.log('Hello Word')
+    console.log('hello!')
+}
+
+//Chama a Função
+HelloWorld()
+
+let teste = HelloWorld()
+
+console.log(teste)
+
+
+function sayHello(name){
+    console.log(`Olá ${name}`)
+}
+
+sayHello('Jõao')
