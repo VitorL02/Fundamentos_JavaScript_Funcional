f1226a3b3ad43019f1d13370299dfbb7b0117e3c
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-12-29 00:12:51 -0300

    Aprendendo sobre filtragem foi criado o arquivo filter_function onde o filter foi aplicado para retornar os alunos que foram aprovados

---
+++ Funcional/filter_function.js
@@ -0,0 +1,26 @@
+numeros = [ 0,1,2,3,4,5,6,7,-1,-2,-3]
+
+const getNumerosPositivos = numeros.filter ((el) => {
+    return el > 0
+});
+
+
+
+
+const menorQueZero = el => el < 0 
+console.log(numeros.filter(menorQueZero));
+console.log(getNumerosPositivos);
+
+const estudandes =[
+    {nome:'Lucas',nota:5.25},
+    {nome:'Clara',nota:7.45},
+    {nome:'Eduardo',nota:5.75},
+    {nome:'Mateus',nota:10.00},
+    {nome:'Jaqueline',nota:6}
+]
+
+const alunosAprovados = el => el.nota >= 6
+
+console.log(
+    estudandes.filter(alunosAprovados)
+)
