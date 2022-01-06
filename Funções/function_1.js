01ed56cc0a7afce8394f322b997c28b89c12db18
Vitor Lucas <vitorcrispim@yahoo.com.br>
2022-01-06 20:30:50 -0300

    Iniciando o novo modulo e relembrando alguns conceitos basicos de funções em javascript foi criado a pasta funções o a arquivo function_1.js

---
+++ Funções/function_1.js
@@ -0,0 +1,26 @@
+
+
+function bomDia(){
+    console.log("Bom Dia")
+}
+
+
+bomDia()
+
+
+//Declarar uma função em uma variavel Function Expression 
+
+const boaTarde = function () {
+    console.log("Boa Tarde")
+}
+
+
+boaTarde()
+
+
+function somar(a,b){
+    return a + b 
+}
+
+const resultado = somar (20,30)
+console.log(resultado)
